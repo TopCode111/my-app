7cca393ec70e7204b416455b1a3e1a989f170799
Alejandrotop <top.tech.prog@gmail.com>
2019-10-27 01:44:31 -0500

    added breweries and httpclient

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:ban-types
+  brews: Object;
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
+    this._http.getBeer().subscribe(data => {
+      this.brews = data;
+      console.log(this.brews);
+    });
   }
 
 }
